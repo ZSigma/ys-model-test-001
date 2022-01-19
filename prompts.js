f73ec7a206171600ec2abb5d320974b0f6432a20
zcw <zengchenwei@eptok.com>
2022-01-19 15:03:11 +0800

    test-5

--- prompts.js
+++ prompts.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 // 读取template文件夹下子文件夹名，一个子文件夹即是一个模板
-const tempList = fs.readdirSync(path.join(__dirname, './template/'), 'utf-8')
+const tempList = fs.readdirSync('./template/')
 console.log('------------------');
 console.log('tempList',tempList)
 module.exports = [
