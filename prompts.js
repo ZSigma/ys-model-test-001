6dc222c267a13c7bc252ff3a265cac01595de520
zcw <zengchenwei@eptok.com>
2022-01-19 11:36:28 +0800

    fix test-4

--- prompts.js
+++ prompts.js
@@ -2,6 +2,8 @@ const fs = require('fs');
 const path = require('path');
 // 读取template文件夹下子文件夹名，一个子文件夹即是一个模板
 const tempList = fs.readdirSync(path.join(__dirname, './template/'), 'utf-8')
+console.log('------------------');
+console.log('tempList',tempList)
 module.exports = [
 {
     type: 'rawlist',
