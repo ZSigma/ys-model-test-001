7d446616b857ebbe26eb8587e6fa02c90c43b607
zcw <zengchenwei@eptok.com>
2022-01-19 17:26:47 +0800

    test-11

--- generator.js
+++ generator.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 module.exports = (api, options, rootOptions) => {
-    const package = fs.readFileSync((path.join(__dirname, `./template/${options.tempName}/package_1.json`),'utf-8'))
+    const package = fs.readFileSync((path.join(__dirname, `./template/${options.tempName}/package_1.json`)),'utf-8')
     api.extendPackage(package);
     console.log('-----------------------------------------------------')
     console.log(options);
