8c2d82581f086c1d08b9285f58d8422d2a659287
zcw <zengchenwei@eptok.com>
2022-01-19 17:55:21 +0800

    test-12

--- generator.js
+++ generator.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const path = require('path');
 module.exports = (api, options, rootOptions) => {
     const package = fs.readFileSync((path.join(__dirname, `./template/${options.tempName}/package_1.json`)),'utf-8')
-    api.extendPackage(package);
+    api.extendPackage(JSON.parse(package));
     console.log('-----------------------------------------------------')
     console.log(options);
     // 复制template模版
